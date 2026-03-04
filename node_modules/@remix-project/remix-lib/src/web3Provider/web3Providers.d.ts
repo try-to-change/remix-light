adacdbcae1945481bf1c6519137cbf2924dfeb3e
Ttoc <106241874+try-to-change@users.noreply.github.com>
2026-03-05 02:21:14 +0800

    Delete node_modules directory

--- node_modules/@remix-project/remix-lib/src/web3Provider/web3Providers.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Web3Providers {
-    modes: any;
-    constructor();
-    addProvider(type: any, obj: any): void;
-    get(type: any, cb: any): any;
-    addWeb3(type: any, web3: any): void;
-    addVM(type: any, vm: any): void;
-}
